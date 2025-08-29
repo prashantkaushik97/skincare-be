f7cf1f475c9c3d1dfc737a856c49598f618ccdac
Prashant Kaushik <kaushik.prashant00@gmail.com>
2025-08-29 14:24:49 -0400

    Healthcheck

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,13 @@ from app.routes.auth import auth_bp
 from app.routes.profile import profile_bp
 from app.routes.routine import routine_bp
 from app.routes.products import products_bp
-
+from app.routes.health import health_bp
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(auth_bp)
     app.register_blueprint(profile_bp)
     app.register_blueprint(routine_bp)
     app.register_blueprint(products_bp)
+    app.register_blueprint(health_bp)
 
     return app
